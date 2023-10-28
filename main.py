fee8f66d5b420461d5c47f7c681049fb2fc33353
Sai-Harshavard3 <22293538-Sai-Harshavard3@users.noreply.replit.com>
2023-10-28 20:01:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import requests
+
+BASE_URL = 'http://api.weatherstack.com/current?'
+CITY = 'Buffalo'
+my_secret1 = os.environ['API_KEY']
+print(my_secret1)
+url = BASE_URL + "access_key=" + my_secret1 + "&query=" + CITY
+response = requests.get(url).json()
+temperature = response["current"]["temperature"]
+print(temperature)
